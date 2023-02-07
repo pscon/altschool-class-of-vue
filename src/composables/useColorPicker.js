f353fa2fe357264aeea8ff96f015dcb1e8f0692e
pscon <ekunolapaul@gmail.com>
2023-02-07 23:18:12 +0100

    added a unique solution for color picker

---
+++ src/composables/useColorPicker.js
@@ -0,0 +1,18 @@
+import { ref, reactive } from "@vue/reactivity";
+
+export function usePicker() {
+  const colors = ref(["green", "red", "blue", "purple"]);
+  let message = ref("Pick a color...");
+
+  const matchColor = (value) => {
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    if (colors[randomNumber] === value) {
+      message.value = `You win... [answer: ${colors[randomNumber]}]`;
+      return;
+    }
+
+    message.value = `You lose [answer: ${colors[randomNumber]}]`;
+  };
+
+  return { colors, message, matchColor };
+}
